fd0fe40bfd9d367c21750b3ef44455f81145a86c
sverevkin25@gmail.com <82106610Vereva>
2019-05-13 17:57:43 +0300

    login

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../shared/services/auth.service";
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,34 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() {
+  form: FormGroup;
+
+  constructor(private auth: AuthService) {
   }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ])
+    })
+  }
+
+  onSubmit() {
+    this.form.disable();
+
+    this.auth.login(this.form.value)
+      .subscribe(
+        () => console.log('success'),
+        error => {
+          console.warn(error);
+          this.form.enable();
+        },
+      ).unsubscribe();
   }
 }
